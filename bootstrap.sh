757f8b22465217362f72c847dbb9b610ff6d074d
Lyon Liang <lyon.liang12@gmail.com>
2015-09-23 05:48:48 +0800

    temp fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,5 @@ export MYSQL_ROOT_PASSWORD=${MYSQL_ENV_MYSQL_ROOT_PASSWORD}
 export MYSQL_HOST=${MYSQL_HOST:-mysql}
 SQL_SCRIPT=/root/keystone.sql
 #mysql -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST <$SQL_SCRIPT
+
+/bin/bash
